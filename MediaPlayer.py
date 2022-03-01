2476b3033bd68039af334fbfdc92d8ac3cbf1426
Mulat <mulatmek@gmail.com>
2022-03-01 13:53:23 +0200

    change load_dir_to_dictionary to load_song_dir_to_dictionary

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -1,43 +1,55 @@
 import re
-import string
-import pygame.mixer
+from pygame import mixer
 import os
 
 
-def load_dir_to_dictionary() -> dict:
+# p = "C:\Users\Mulat\Desktop\MediaPlayerProject\songs"
+
+
+def load_song_dir_to_dictionary() -> dict:
     user_path = input()
     songs_name = os.listdir(user_path)
+    files_path = [os.path.abspath(user_path) for x in os.listdir(user_path)]
+    for i in range(0, len(files_path)):
+        files_path[i] = files_path[i] + os.sep + songs_name[i]
     for i in range(0, len(songs_name)):
         songs_name[i] = make_file_pretty_name(songs_name[i])
-    files_path = [os.path.abspath(x) for x in os.listdir(user_path)]
     return dict(zip(songs_name, files_path))
 
 
-def make_file_pretty_name(song_name) -> string:
+def make_file_pretty_name(song_name):
     string_array = re.split('[-_ ]', song_name)
     return string_array[0] + " " + string_array[1]
 
 
-def playMusic():
-    pass
-
-
 def play(file_name):
-    pygame.mixer.init()
-    pygame.mixer.music.load(file_name)
-    pygame.mixer.music.play()
+    mixer.init()
+    mixer.music.load(file_name)
+    mixer.music.play()
+    while True:
+        print("Press 'p' to pause, 'r' to resume")
+        print("Press 'e' to exit the program")
+        query = input("  ")
 
-
-#    return pygame.mixer.Sound.get_length()
+        if query == 'p':
+            # Pausing the music
+            pause()
+        if query == 'r':
+            # Resuming the music
+            resume()
+        if query == 'e':
+            # Stop the mixer
+            stop()
+            break
 
 
 def pause():
-    pygame.mixer.music.pause()
+    mixer.music.pause()
 
 
 def resume():
-    pygame.mixer.music.unpause()
+    mixer.music.unpause()
 
 
 def stop():
-    pygame.mixer.music.stop()
+    mixer.music.stop()
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import MediaPlayer
 import os
 songs_list=MediaPlayer.load_dir_to_dictionary()
 print(songs_list)
+MediaPlayer.play(songs_list["Bruno Mars"])
