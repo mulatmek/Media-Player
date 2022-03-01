9aa78042067dd232b64a3893a692be86c1974b1d
Mulat <mulatmek@gmail.com>
2022-03-01 12:24:02 +0200

    overide load dir to dict

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -1,5 +1,5 @@
+import re
 import string
-
 import pygame.mixer
 import os
 
@@ -7,27 +7,28 @@ import os
 def load_dir_to_dictionary() -> dict:
     user_path = input()
     songs_name = os.listdir(user_path)
-    for i in range(0,len(songs_name)):
-        songs_name[i]=make_file_pretty_name(songs_name[i])
-    print(songs_name)
+    for i in range(0, len(songs_name)):
+        songs_name[i] = make_file_pretty_name(songs_name[i])
     files_path = [os.path.abspath(x) for x in os.listdir(user_path)]
-    zip_iterator = zip(songs_name, files_path)
-    return dict(zip_iterator)
+    return dict(zip(songs_name, files_path))
 
 
 def make_file_pretty_name(song_name) -> string:
-    return song_name.split("-")[0]
+    string_array = re.split('[-_ ]', song_name)
+    return string_array[0] + " " + string_array[1]
 
 
 def playMusic():
     pass
 
 
-def play(file_name) -> float:
+def play(file_name):
     pygame.mixer.init()
     pygame.mixer.music.load(file_name)
     pygame.mixer.music.play()
-    return pygame.mixer.Sound.get_length()
+
+
+#    return pygame.mixer.Sound.get_length()
 
 
 def pause():
---
+++ main.py
@@ -0,0 +1,4 @@
+import MediaPlayer
+import os
+songs_list=MediaPlayer.load_dir_to_dictionary()
+print(songs_list)
