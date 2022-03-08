4382cc6c7b3103449a81979295a4c27d0a5435e5
Mulat <mulatmek@gmail.com>
2022-03-08 21:18:28 +0200

    change load_song_dir_to_dictionary,make_file_pretty_name remove pause ,resume ,stop from MediaPlayer.py

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -4,50 +4,28 @@ import os
 
 
 # p = "C:\Users\Mulat\Desktop\MediaPlayerProject\songs"
-
+# click ->
 
 def load_song_dir_to_dictionary(user_path) -> dict:
-    song_names = os.listdir(user_path)
-    files_path = [os.path.abspath(user_path) for x in os.listdir(user_path)]
-    for i in range(0, len(files_path)):
-        files_path[i] = files_path[i] + os.sep + song_names[i]
-    for i in range(0, len(song_names)):
-        song_names[i] = make_file_pretty_name(song_names[i])
-    return dict(zip(song_names, files_path))
+    root_path = os.path.abspath(user_path)
+    return {
+        make_file_pretty_name(file): root_path + os.sep + file
+        for file in os.listdir(user_path)
+    }
 
 
 def make_file_pretty_name(song_name):
-    string_array = re.split('[-_ ]', song_name)
-    return string_array[0] + " " + string_array[1]
+    res=""
+    for string in song_name.split()[:7]:
+        res+=string
+        res+=" "
+    return res.removesuffix(" ")
+
+
+# test
+#assert make_file_pretty_name("Drake - Fake Love (128  kbps) (mp3conv.ru).mp3") == "Drake "
 
 
 def play(file_name):
     mixer.music.load(file_name)
     mixer.music.play()
-    while True:
-        print("Press 'p' to pause, 'r' to resume")
-        print("Press 'e' to exit the program")
-        query = input("  ")
-
-        if query == 'p':
-            # Pausing the music
-            pause()
-        if query == 'r':
-            # Resuming the music
-            resume()
-        if query == 'e':
-            # Stop the mixer
-            stop()
-            break
-
-
-def pause():
-    mixer.music.pause()
-
-
-def resume():
-    mixer.music.unpause()
-
-
-def stop():
-    mixer.music.stop()
